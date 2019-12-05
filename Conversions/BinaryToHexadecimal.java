d7f6fc9cfc8625c88a70f7a73b4b9e201311250e
PYO-SE-SEUNG <97ss93sh@gmail.com>
2019-12-05 12:07:15 +0900

    Update BinaryToHexadecimal.java
    BinaryToHexadecimal 번역 및 코드 코멘트

--- Conversions/BinaryToHexadecimal.java
+++ Conversions/BinaryToHexadecimal.java
@@ -3,23 +3,22 @@ package Conversions;
 import java.util.*;
 
 /**
- * Converts any Binary Number to a Hexadecimal Number
+ * 2진수를 16진수로 변환한다.
  *
  * @author Nishita Aggarwal
  */
 public class BinaryToHexadecimal {
 
     /**
-     * This method converts a binary number to
-     * a hexadecimal number.
+     * 밑의 함수들은 2진수를 16진수의 숫자로 변환하는 방법에 관한 것이다.
      *
      * @param binary The binary number
      * @return The hexadecimal number
      */
     static String binToHex(int binary) {
-        //hm to store hexadecimal codes for binary numbers within the range: 0000 to 1111 i.e. for decimal numbers 0 to 15
+        //해시맵은 16진수의 코드를 2진수와 맵핑시키기 위해서 사용한다 (예시 : 0 -> 0000, 15 -> 1111)
         HashMap<Integer, String> hm = new HashMap<>();
-        //String to store hexadecimal code
+        //16진수의 숫자를 string으로 저장
         String hex = "";
         int i;
         for (i = 0; i < 10; i++) {
@@ -28,7 +27,7 @@ public class BinaryToHexadecimal {
         for (i = 10; i < 16; i++) hm.put(i, String.valueOf((char) ('A' + i - 10)));
         int currbit;
         while (binary != 0) {
-            int code4 = 0;    //to store decimal equivalent of number formed by 4 decimal digits
+            int code4 = 0;    // to store decimal equivalent of number formed by 4 decimal digits
             for (i = 0; i < 4; i++) {
                 currbit = binary % 10;
                 binary = binary / 10;
@@ -53,3 +52,4 @@ public class BinaryToHexadecimal {
         sc.close();
     }
 }
+// 이진수를 정수의 형태로 입력받고(0 과 1만을 이용하여 입력받음), 이를 문자열 형태의 16진수로 변환하여 출력한다.
