3eb9cb6a3c79d20edbd38a08807bf5c6bd268373
PYO-SE-SEUNG <97ss93sh@gmail.com>
2019-12-05 11:46:06 +0900

    Update BinaryToDecimal.java
    Exception 추가

--- Conversions/BinaryToDecimal.java
+++ Conversions/BinaryToDecimal.java
@@ -16,16 +16,21 @@ class BinaryToDecimal {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int binNum, binCopy, d, s = 0, power = 0;
-        System.out.print("Binary number: ");
-        binNum = sc.nextInt();
-        binCopy = binNum;
-        while (binCopy != 0) {
-            d = binCopy % 10;
-            s += d * (int) Math.pow(2, power++);
-            binCopy /= 10;
+        try{
+            
+            System.out.print("Binary number: ");
+            binNum = sc.nextInt();
+            binCopy = binNum;
+            while (binCopy != 0) {
+                d = binCopy % 10;
+                s += d * (int) Math.pow(2, power++);
+                binCopy /= 10;
+            }
+            System.out.println("Decimal equivalent:" + s);
+            sc.close();
+        }catch(){
+        
         }
-        System.out.println("Decimal equivalent:" + s);
-        sc.close();
     }
 }
 // 매우 정석적인 코드이고, 알고리즘을 처음 접하는 사람들에게 천천히 코드를 읽어보면서 변환되는 과정을 음미해볼 것을 권한다.
