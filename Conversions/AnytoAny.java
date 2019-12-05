cc13f0115de71fffcceb4939da4f0630e550d8f8
PYO-SE-SEUNG <97ss93sh@gmail.com>
2019-12-05 11:43:09 +0900

    Update AnytoAny.java
    AnyToAny conversion 번역 및 코드 코멘트

--- Conversions/AnytoAny.java
+++ Conversions/AnytoAny.java
@@ -1,16 +1,16 @@
 package Conversions;
 
 import java.util.Scanner;
-//given a source number , source base, destination base, this code can give you the destination number.
+//주어진 숫자와 초기진법, 최종진법을 고려하여 초기진법으로 주어진 숫자를 최종 진법의 결과 숫자로 변환한다.
 //sn ,sb,db ---> ()dn  .   this is what we have to do    .
 
 public class AnytoAny {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int sn = scn.nextInt();
-        int sb = scn.nextInt();
-        int db = scn.nextInt();
+        int sn = scn.nextInt(); //변환하고자하는 숫자
+        int sb = scn.nextInt(); //변환하고자하는 숫자의 진법
+        int db = scn.nextInt(); //최종적으로 변환되어져 나올 숫자의 진법
         int m = 1, dec = 0, dn = 0;
         while (sn != 0) {
             dec = dec + (sn % 10) * m;
@@ -27,3 +27,6 @@ public class AnytoAny {
     }
 
 }
+
+// AnyBaseToAnyBase.java 의 코드보다 좀 더 간결하다. 하지만, 알파벳으로 표현되는 숫자를 처리할 수 없는 것이 단점이다.
+// 10진수 이하의 진법으로 표현된 숫자들끼리만 변환이 
