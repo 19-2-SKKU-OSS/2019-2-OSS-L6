d8f51d79c04acd108defc99cfb75a2e4b4762420
PYO-SE-SEUNG <97ss93sh@gmail.com>
2019-12-05 12:58:02 +0900

    Update IntegerToRoman.java
    IntegerToRoman conversion 번역 및 코드 코멘트

--- Conversions/IntegerToRoman.java
+++ Conversions/IntegerToRoman.java
@@ -14,16 +14,17 @@ public class IntegerToRoman {
         for (int a = 0; a < allArabianRomanNumbers.length; a++) {
             int times = num / allArabianRomanNumbers[a];
             for (int b = 0; b < times; b++) {
-                builder.append(allRomanNumbers[a]);
+                builder.append(allRomanNumbers[a]); //숫자에 맞는 알파벳을 문자열에 이어붙인다.
             }
 
             num -= times * allArabianRomanNumbers[a];
         }
 
-        return builder.toString();
+        return builder.toString(); //최종적으로 만들어진 숫자를 반환한다.
     }
 
     public static void main(String[] args) {
         System.out.println(IntegerToRoman.integerToRoman(2131));
     }
 }
+// 10진수의 입력값은 로마문자를 이용한 숫자형태로 표현한다.
