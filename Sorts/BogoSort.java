22101bb36c336e221e84289120cb0abb6b76b001
qkrwlgns0605 <qkrwlgns0605@gmail.com>
2019-12-06 17:51:36 +0900

    add explaining

--- Sorts/BogoSort.java
+++ Sorts/BogoSort.java
@@ -6,6 +6,8 @@ import java.util.Random;
 /**
  * @author Podshivalov Nikita (https://github.com/nikitap492)
  * @see SortAlgorithm
+ * 보고 정렬: 이름 그대로 멍청한 정렬. 랜덤으로 데이터들을 재배열한 후, 정렬되었는지 검사한다. 정렬이 되지 않았으면 다시 랜덤으로 정렬될 때까지 재배열한다.
+ * 평균 시간복잡도: O(n*n!), 최악의 경우: O(∞)
  */
 public class BogoSort implements SortAlgorithm {
 
