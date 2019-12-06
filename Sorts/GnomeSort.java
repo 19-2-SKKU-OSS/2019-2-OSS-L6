8c0524fe04e4b9507440d825b5aa105b8da4a553
qkrwlgns0605 <qkrwlgns0605@gmail.com>
2019-12-07 00:47:31 +0900

    Update GnomeSort.java

--- Sorts/GnomeSort.java
+++ Sorts/GnomeSort.java
@@ -7,24 +7,26 @@ import static Sorts.SortUtils.*;
  *
  * @author Podshivalov Nikita (https://github.com/nikitap492)
  * @since 2018-04-10
- **/
-public class GnomeSort implements SortAlgorithm {
+ * 난쟁이 정렬: 두 값을 비교하여 교환이 필요하면 교환하고 index를 하나 낮추고, 필요하지 않다면 index를 하나 증가하는 것을 반복하며 정렬.
+ * 시간복잡도: O(n^2), 최악의 경우: O(n^2), 최선의 경우: O(n)
+ * */
+public class GnomeSort implements SortAlgorithm { //GnomeSort 클래스 정의
 
     @Override
     public <T extends Comparable<T>> T[] sort(T[] arr) {
         int i = 1;
         int j = 2;
-        while (i < arr.length) {
-            if (less(arr[i - 1], arr[i])) i = j++;
-            else {
-                swap(arr, i - 1, i);
-                if (--i == 0) {
-                    i = j++;
+        while (i < arr.length) { //배열의 크기만큼
+            if (less(arr[i - 1], arr[i])) i = j++; //i-1 인덱스의 값과 i의 값을 비교하여 앞의 값이 작으면 인덱스를 하나 증가시킴
+            else { //앞의 값이 크면 -> 오름차순 정렬
+                swap(arr, i - 1, i); //스왑을 해주고
+                if (--i == 0) { //인덱스를 하나 낮춰줌. 그리고 그것이 만약 0이면
+                    i = j++; //다음 작업을 위해 j의 값을 넣어주고 j 증가
                 }
             }
         }
 
-        return null;
+        return null; //배열은 리턴하지 않음
     }
 
     public static void main(String[] args) {
