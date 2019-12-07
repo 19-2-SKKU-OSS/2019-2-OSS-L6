35d9059e6cc8dfe1134a3b43f4b9eff568e254dc
qkrwlgns0605 <qkrwlgns0605@gmail.com>
2019-12-08 04:01:15 +0900

    Update PowerOfTwoOrNot.java

--- Others/PowerOfTwoOrNot.java
+++ Others/PowerOfTwoOrNot.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 /**
  * A utility to check if a given number is power of two or not.
  * For example 8,16 etc.
+ * 2의 거듭제곱 판별 알고리즘: 수가 2의 거듭제곱인지 아닌지 비트 연산자를 사용하여 찾는 알고리즘
  */
 
 public class PowerOfTwoOrNot {
@@ -30,7 +31,7 @@ public class PowerOfTwoOrNot {
      * @return boolean
      */
     public static boolean checkIfPowerOfTwoOrNot(int number) {
-        return number != 0 && ((number & (number - 1)) == 0);
+        return number != 0 && ((number & (number - 1)) == 0); //0이 아니고, 수에서 하나 뺀 값과의 & 연산을 사용했을 때 0이 아니라면 true 리턴, 아니면 false 리턴
     }
 
 }
