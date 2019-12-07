e83519c3134fc4ba11f34fdd1e968d0e05b3f71d
qkrwlgns0605 <qkrwlgns0605@gmail.com>
2019-12-07 18:06:56 +0900

    Update SortAlgorithm.java

--- Sorts/SortAlgorithm.java
+++ Sorts/SortAlgorithm.java
@@ -8,7 +8,7 @@ import java.util.List;
  *
  * @author Podshivalov Nikita (https://github.com/nikitap492)
  **/
-public interface SortAlgorithm {
+public interface SortAlgorithm { //SortAlgorithm 인터페이스 정의
 
     /**
      * Main method arrays sorting algorithms
@@ -16,13 +16,13 @@ public interface SortAlgorithm {
      * @param unsorted - an array should be sorted
      * @return a sorted array
      */
-    <T extends Comparable<T>> T[] sort(T[] unsorted);
+    <T extends Comparable<T>> T[] sort(T[] unsorted); //정렬하는 함수
 
     /**
      * Auxiliary method for algorithms what wanted to work with lists from JCF
      *
-     * @param unsorted - a list should be sorted
-     * @return a sorted list
+     * @param unsorted - a list should be sorted //정렬해야할 함수를 가져야함
+     * @return a sorted list //정렬된 함수를 리턴해야함
      */
     @SuppressWarnings("unchecked")
     default <T extends Comparable<T>> List<T> sort(List<T> unsorted) {
