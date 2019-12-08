ba5d9623fc86d039d9348eb07fcb93b6fa6f677d
jaemin <fhqht7@gmail.com>
2019-12-08 09:31:30 -0800

    convert Factorial.java

--- Maths/Factorial.java
+++ Maths/Factorial.java
@@ -7,10 +7,10 @@ public class Factorial {
     }
 
     /**
-     * Calculate factorial
+     * 팩토리얼을 계산합니다
      *
-     * @param n the number
-     * @return the factorial of {@code n}
+     * @인자로 숫자 n을 받습니다
+     * @n의 팩토리얼을 반환합니다
      */
     public static long factorial(int n) {
         if (n < 0) {
