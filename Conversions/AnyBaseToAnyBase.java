870192e761a6f0d387eb2769da3d533939cb75a1
PYO-SE-SEUNG <97ss93sh@gmail.com>
2019-12-08 21:08:37 +0900

    Update AnyBaseToAnyBase.java
    AnyBaseToAnyBase 번역 및 코드 코멘트

--- Conversions/AnyBaseToAnyBase.java
+++ Conversions/AnyBaseToAnyBase.java
@@ -7,8 +7,7 @@ import java.util.Scanner;
 
 /**
  * 특정 진법에서 또다른 특정 진법으로 수를 변환하는데 사용할 수 있는 클래스이다. 여기서 '특정 진법'이란 2에서 36까지를 말한다.
- * Works by going from base 1 to decimal to base 2. Includes auxiliary method for
- * determining whether a number is valid for a given base.
+ * 특정 진법 b1에서 특정 진법 b2로 변환된다. 주어진 입력 숫자가 주어진 진법에 대해 유효한지 판단하는 보조 방법을 포함한다.
  *
  * @author Michael Rolland
  * @version 2017.10.10
@@ -62,15 +61,15 @@ public class AnyBaseToAnyBase {
         char[] validDigits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E',
                 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
                 'W', 'X', 'Y', 'Z'};
-        // digitsForBase contains all the valid digits for the base given
+        // digitsForBase는 진법에 사용할 수 있는 모든 유효한 숫자와 문자를 포함한다.
         char[] digitsForBase = Arrays.copyOfRange(validDigits, 0, base);
 
-        // Convert character array into set for convenience of contains() method
+        // 문자 배열을 contains() 함수의 편의를 위해 해시 셋으로 변환한다.
         HashSet<Character> digitsList = new HashSet<>();
         for (int i = 0; i < digitsForBase.length; i++)
             digitsList.add(digitsForBase[i]);
 
-        // Check that every digit in n is within the list of valid digits for that base.
+        // n의 모든 문자가 주어진 진법에 대해 유효한 자릿수인지를 체크한다.
         for (char c : n.toCharArray())
             if (!digitsList.contains(c))
                 return false;
@@ -79,56 +78,53 @@ public class AnyBaseToAnyBase {
     }
 
     /**
-     * Method to convert any integer from base b1 to base b2. Works by converting from b1 to decimal,
-     * then decimal to b2.
+     * 아래의 함수는 b1의 진법으로 나타낸 수를 b2의 진법으로 변환하는 함수이다. 먼저 b1의 진법으로 나타낸 수를 10진수로 변환한 후, 
+     * 이를 b2의 진법으로 변환한다.
      *
-     * @param n  The integer to be converted.
-     * @param b1 Beginning base.
-     * @param b2 End base.
-     * @return n in base b2.
+     * @param n  변환될 정수
+     * @param b1 시작 진법
+     * @param b2 최종 진법
+     * @n을 진법 b2의 수로 반환한다.
      */
     public static String base2base(String n, int b1, int b2) {
-        // Declare variables: decimal value of n,
-        // character of base b1, character of base b2,
-        // and the string that will be returned.
+        // 변수선언 : n의 10진수 값, 진법 b1를 나타내는 문자, 
+        // 진법 b2를 나타내는 문자, 변환되어 반환될 문자열
         int decimalValue = 0, charB2;
         char charB1;
         String output = "";
-        // Go through every character of n
+        // n의 모든 자리의 문자에 대해서
         for (int i = 0; i < n.length(); i++) {
-            // store the character in charB1
+            // charB1에 각 자리 문자를 저장한다.
             charB1 = n.charAt(i);
-            // if it is a non-number, convert it to a decimal value >9 and store it in charB2
+            // 만약에 문자가 숫자가 아니라면(A-Z), 이를 9보다 큰 10진수로 변환하여 charB2에 저장한다.
             if (charB1 >= 'A' && charB1 <= 'Z')
                 charB2 = 10 + (charB1 - 'A');
-                // Else, store the integer value in charB2
+                // 그렇지 않을 경우, charB2에 정수 값을 저장한다.
             else
                 charB2 = charB1 - '0';
-            // Convert the digit to decimal and add it to the
-            // decimalValue of n
+            // 각 숫자 문자를 10진수의 정수 형태로 변환하고 이를 n의 decimalValue에 저장한다.
             decimalValue = decimalValue * b1 + charB2;
         }
 
-        // Converting the decimal value to base b2:
-        // A number is converted from decimal to another base
-        // by continuously dividing by the base and recording
-        // the remainder until the quotient is zero. The number in the
-        // new base is the remainders, with the last remainder
-        // being the left-most digit.
+        // 10진수 의 값을 진법 b2의 수로 변환한다.
+        // 연속적으로 몫이 0이 되기전까지 몫을 새로운 진법의 수로 나누고, 나눌때마다 나머지를 저장하는 것을 통해서
+        // 숫자는 10진수에서 다른 진법의 숫자로 변환된다.
+        // 새로운 진법의 수는 나머지에 해당하고, 마지막 나머지가 새로운 숫자의 가장 왼편에 위치한다.
 
-        // While the quotient is NOT zero:
+        // 몫이 0이 아닐 동안 
         while (decimalValue != 0) {
-            // If the remainder is a digit < 10, simply add it to
-            // the left side of the new number.
+            // 만약 나머지가 10보다 작은 수라면 간단히 새로운 숫자의 가장 왼편에 추가한다.
             if (decimalValue % b2 < 10)
                 output = Integer.toString(decimalValue % b2) + output;
-                // If the remainder is >= 10, add a character with the
-                // corresponding value to the new number. (A = 10, B = 11, C = 12, ...)
+                // 만약 나머지가 10보다 큰 수라면 숫자에 알맞는 문자를 새로운 숫자의 가장 왼편에 추가한다.
+                // (A = 10, B = 11, C = 12, ...)
             else
                 output = (char) ((decimalValue % b2) + 55) + output;
-            // Divide by the new base again
+            // 새로운 진법의 수로 다시 나눈다.
             decimalValue /= b2;
         }
         return output;
     }
 }
+//사용자가 직접 두 개의 시작 진법과 최종 진법을 선택하여 시작 진법으로 나타낸 숫자를 최종 진법의 수로 변환하는 알고리즘이다.
+//동일한 목적을 가진 또다른 구현이 존재하므로, 여러개의 같은 목적을 가진 알고리즘을 비교하면서 이해하는 것을 추천한다.
