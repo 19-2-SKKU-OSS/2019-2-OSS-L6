f31388fbf4e329e699ae72c29c011a5e1901b7be
bidulki <55395688+bidulki@users.noreply.github.com>
2019-12-08 17:21:08 +0900

    translate and correct the comments

--- DataStructures/Heaps/MinHeap.java
+++ DataStructures/Heaps/MinHeap.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Heap tree where a node's key is higher than or equal to its parent's and lower than or equal
- * to its children's.
+ * 노드의 키가 상위 키보다 작거나 같고 하위 키보다 나 같은 힙 트리
  *
  * @author Nicolas Renard
  */
@@ -22,26 +21,26 @@ public class MinHeap implements Heap {
         if (minHeap.size() == 0) System.out.println("No element has been added, empty heap.");
     }
 
-    // Get the element at a given index. The key for the list is equal to index value - 1
+    // 주어진 인덱스에 있는 요소를 얻는다. 리스트의 키는 인덱스 값-1과 동일하다
     public HeapElement getElement(int elementIndex) {
         if ((elementIndex <= 0) || (elementIndex > minHeap.size()))
             throw new IndexOutOfBoundsException("Index out of heap range");
         return minHeap.get(elementIndex - 1);
     }
 
-    // Get the key of the element at a given index
+    // 주어진 인덱스에 있는 요소를 얻는다
     private double getElementKey(int elementIndex) {
         return minHeap.get(elementIndex - 1).getKey();
     }
 
-    // Swaps two elements in the heap
+    // 힙에 있는 두 요소를 바꾼다
     private void swap(int index1, int index2) {
         HeapElement temporaryElement = minHeap.get(index1 - 1);
         minHeap.set(index1 - 1, minHeap.get(index2 - 1));
         minHeap.set(index2 - 1, temporaryElement);
     }
 
-    // Toggle an element up to its right place as long as its key is lower than its parent's
+    // 키가 부모의 키보다 높은 한, 원소를 그것의 윗 자리로 전환한다.
     private void toggleUp(int elementIndex) {
         double key = minHeap.get(elementIndex - 1).getKey();
         while (getElementKey((int) Math.floor(elementIndex / 2)) > key) {
@@ -50,13 +49,12 @@ public class MinHeap implements Heap {
         }
     }
 
-    // Toggle an element down to its right place as long as its key is higher
-    // than any of its children's
+    // 키가 자식의 키보다 낮은 한, 원소를 그것의 아래 자리로 전환한다.
     private void toggleDown(int elementIndex) {
         double key = minHeap.get(elementIndex - 1).getKey();
         boolean wrongOrder = (key > getElementKey(elementIndex * 2)) || (key > getElementKey(Math.min(elementIndex * 2, minHeap.size())));
         while ((2 * elementIndex <= minHeap.size()) && wrongOrder) {
-            // Check whether it shall swap the element with its left child or its right one if any.
+            // 왼쪽 자식와 교환할지 오른쪽 자식과 교환할지 검사한다.
             if ((2 * elementIndex < minHeap.size()) && (getElementKey(elementIndex * 2 + 1) < getElementKey(elementIndex * 2))) {
                 swap(elementIndex, 2 * elementIndex + 1);
                 elementIndex = 2 * elementIndex + 1;
@@ -92,12 +90,12 @@ public class MinHeap implements Heap {
             }
         if ((elementIndex > minHeap.size()) || (elementIndex <= 0))
             throw new IndexOutOfBoundsException("Index out of heap range");
-        // The last element in heap replaces the one to be deleted
+        // 힙의 마지막 요소를 삭제할 요소로 대체한다.
         minHeap.set(elementIndex - 1, getElement(minHeap.size()));
         minHeap.remove(minHeap.size());
-        // Shall the new element be moved up...
+        // 새로운 요소를 위로 이동시킬지
         if (getElementKey(elementIndex) < getElementKey((int) Math.floor(elementIndex / 2))) toggleUp(elementIndex);
-            // ... or down ?
+            // 아래로 이동시킬지
         else if (((2 * elementIndex <= minHeap.size()) && (getElementKey(elementIndex) > getElementKey(elementIndex * 2))) ||
                 ((2 * elementIndex < minHeap.size()) && (getElementKey(elementIndex) > getElementKey(elementIndex * 2))))
             toggleDown(elementIndex);
