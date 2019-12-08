14d7a3361764f1c0e2aa455446e10659f6031a12
bidulki <55395688+bidulki@users.noreply.github.com>
2019-12-08 16:46:54 +0900

    translate about what is heap

--- DataStructures/Heaps/Heap.java
+++ DataStructures/Heaps/Heap.java
@@ -1,15 +1,13 @@
 package DataStructures.Heaps;
 
 /**
- * Interface common to heap data structures.<br>
- * <p>Heaps are tree-like data structures that allow storing elements in a specific
- * way. Each node corresponds to an element and has one parent node (except for the root) and
- * at most two children nodes. Every element contains a key, and those keys
- * indicate how the tree shall be built. For instance, for a min-heap, the key of a node shall
- * be greater than or equal to its parent's and lower than or equal to its children's (the opposite rule applies to a
- * max-heap).</p>
- * <p>All heap-related operations (inserting or deleting an element, extracting the min or max) are performed in
- * O(log n) time.</p>
+ * 힙 데이터와 공통된 인터페이스<br>
+ * <p>힙은 트리와 비슷한 데이터 구조로서, 요소를 특정한 방법으로 저장할 수 있다.
+ * 각 노드는 한 요소에 해당하며(루트를 제외하면) 하나의 부모 노드와 최대 두 개의 자식 노드를 가지고 있다.
+ * 모든 요소에는 키가 들어 있고, 그 키들은 트리를 어떻게 만들어야하는 지를 나타낸다.
+ * 예를 들어, 최소 힙의 경우, 노드의 키는 부모 노드의 키보다 크거나 같아야 하고 자식 노드의 키보다 작거나 같아야 한다.
+ * 최대 힙에는 반대의 규칙이 적용된다.</p>
+ * <p>모든 힙 관련 작업(요소 삽입 또는 삭제, 최소 또는 최대 추출)은 O(log n)의 시간 복잡도를 가진다.</p>
  *
  * @author Nicolas Renard
  */
