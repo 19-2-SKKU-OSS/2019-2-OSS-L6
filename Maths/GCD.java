8894adc6fb04b313a310d519b51a52441db60842
jaemin <fhqht7@gmail.com>
2019-12-08 12:49:30 -0800

    edit description for GCD.java

--- Maths/GCD.java
+++ Maths/GCD.java
@@ -2,18 +2,18 @@ package Maths;
 
 /**
  * 최대공약수를 찾는 유클리드 알고리즘 입니다
- * 함수 gcd는 overide를 사용하였습니다 
+ * 함수 gcd(최대공약수)는 overide를 사용하였습니다 
  *
  * @author Oskar Enmalm 3/10/17
  */
 public class GCD {
 
     /**
-     * get greatest common divisor
+     * 최대공약수를 구합시다
      *
-     * @param num1 the first number
-     * @param num2 the second number
-     * @return gcd
+     * @인자 num1은 첫 번째 숫자입니다
+     * @인자 num2는 두 번째 숫자입니다
+     * @gcd를 반환합니다
      */
     public static int gcd(int num1, int num2) {
         if (num1 < 0 || num2 < 0) {
@@ -33,10 +33,10 @@ public class GCD {
     }
 
     /**
-     * get greatest common divisor in array
+     * 배열에서 최대공약수를 찾읍시다
      *
-     * @param number contains number
-     * @return gcd
+     * @인자로 배열에 저장된 숫자들을 받습니다
+     * @gcd를 반환합니다
      */
     public static int gcd(int[] number) {
         int result = number[0];
@@ -50,7 +50,7 @@ public class GCD {
     public static void main(String[] args) {
         int[] myIntArray = {4, 16, 32};
 
-        // call gcd function (input array)
+        // gcd 함수를 호출합시다 (배열을 넣고)
         System.out.println(gcd(myIntArray)); // => 4
         System.out.printf("gcd(40,24)=%d gcd(24,40)=%d\n", gcd(40, 24), gcd(24, 40)); // => 8
     }
