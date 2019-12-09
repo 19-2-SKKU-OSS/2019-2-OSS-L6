4e56dfdce793d6811ce6c4fb3f4809ed1bc89f44
bidulki <55395688+bidulki@users.noreply.github.com>
2019-12-09 17:59:56 +0900

    translate comments

--- DataStructures/Queues/PriorityQueues.java
+++ DataStructures/Queues/PriorityQueues.java
@@ -1,32 +1,31 @@
 package DataStructures.Queues;
 
 /**
- * This class implements a PriorityQueue.
+ * 이 클래스는 우선순위 큐를 구현한 것이다.
  * <p>
- * A priority queue adds elements into positions based on their priority.
- * So the most important elements are placed at the front/on the top.
- * In this example I give numbers that are bigger, a higher priority.
- * Queues in theory have no fixed size but when using an array
- * implementation it does.
+ * 우선순위 큐는 그들의 우선순위에 기반해서 요소를 추가한다.
+ * 그래서 가장 중요한 요소는 제일 위에 둔다.
+ * 예시에서 더 높은 우선순위를 가진 수를 제공할 것이다.
+ * Queues 는 이론상 고정된 크기를 가지진 않지만 배열로 구현할 경우는 그러하다. 
  */
 class PriorityQueue {
     /**
-     * The max size of the queue
+     * queue의 최대 크기
      */
     private int maxSize;
     /**
-     * The array for the queue
+     * queue를 위한 배열
      */
     private int[] queueArray;
     /**
-     * How many items are in the queue
+     * queue에 들어있는 아이템의 개수
      */
     private int nItems;
 
     /**
-     * Constructor
+     * 생성자
      *
-     * @param size Size of the queue
+     * @param size queue의 크기
      */
     public PriorityQueue(int size) {
         maxSize = size;
@@ -35,64 +34,64 @@ class PriorityQueue {
     }
 
     /**
-     * Inserts an element in it's appropriate place
+     * 적절한 장소에 요소 삽입
      *
-     * @param value Value to be inserted
+     * @param value 삽입될 값
      */
     public void insert(int value) {
         if (isFull()) {
             throw new RuntimeException("Queue is full");
         } else {
-            int j = nItems - 1; // index of last element
+            int j = nItems - 1; // 마지막 요소의 인덱스
             while (j >= 0 && queueArray[j] > value) {
-                queueArray[j + 1] = queueArray[j]; // Shifts every element up to make room for insertion
+                queueArray[j + 1] = queueArray[j]; // 추가하기 위해 모든 요소의 자리를 바꾼다.
                 j--;
             }
-            queueArray[j + 1] = value; // Once the correct position is found the value is inserted
+            queueArray[j + 1] = value; // 값이 들어갈 적절한 장소를 찾은 경우
             nItems++;
         } 
     }
 
     /**
-     * Remove the element from the front of the queue
+     * Remove queue 앞에 있는 요소 삭제
      *
-     * @return The element removed
+     * @return 삭제된 요소
      */
     public int remove() {
         return queueArray[--nItems];
     }
 
     /**
-     * Checks what's at the front of the queue
+     * queue의 앞에 무엇이 있는지 검사한다.
      *
-     * @return element at the front of the queue
+     * @return queue의 앞에있는 요소
      */
     public int peek() {
         return queueArray[nItems - 1];
     }
 
     /**
-     * Returns true if the queue is empty
+     * queue가 비어있을 경우 참을 반환
      *
-     * @return true if the queue is empty
+     * @return queue가 비어있을 경우
      */
     public boolean isEmpty() {
         return (nItems == 0);
     }
 
     /**
-     * Returns true if the queue is full
+     * queue가 가득 차있을 경우 참을 반환
      *
-     * @return true if the queue is full
+     * @return queue가 가득 차있을 경우 참
      */
     public boolean isFull() {
         return (nItems == maxSize);
     }
 
     /**
-     * Returns the number of elements in the queue
+     * queue에 있는 요소의 개수를 반환
      *
-     * @return number of elements in the queue
+     * @return queue에 있는 요소의 개수
      */
     public int getSize() {
         return nItems;
@@ -100,15 +99,15 @@ class PriorityQueue {
 }
 
 /**
- * This class implements the PriorityQueue class above.
+ * 우선순위 큐 구현을 위한 클래스
  *
  * @author Unknown
  */
 public class PriorityQueues {
     /**
-     * Main method
+     * 메인메소드
      *
-     * @param args Command Line Arguments
+     * @param args 커맨드 라인
      */
     public static void main(String[] args) {
         PriorityQueue myQueue = new PriorityQueue(4);
@@ -116,11 +115,11 @@ public class PriorityQueues {
         myQueue.insert(2);
         myQueue.insert(5);
         myQueue.insert(3);
-        // [2, 3, 5, 10] Here higher numbers have higher priority, so they are on the top
+        // [2, 3, 5, 10] 높은 수일 수록 높은 우선순위를 갖도록 하였다.
 
         for (int i = 3; i >= 0; i--)
-            System.out.print(myQueue.remove() + " "); // will print the queue in reverse order [10, 5, 3, 2]
+            System.out.print(myQueue.remove() + " "); // queue를 반대 순서로 출력한다. [10, 5, 3, 2]
 
-        // As you can see, a Priority Queue can be used as a sorting algotithm
+        // 보다시피 우선순위 큐는 정렬 알고리즘에 쓰일 수 있다.
     }
 }
