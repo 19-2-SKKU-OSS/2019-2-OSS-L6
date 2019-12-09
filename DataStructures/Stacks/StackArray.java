836d5b69a72a4f63b2eb002cc800c8cef9e74612
bidulki <55395688+bidulki@users.noreply.github.com>
2019-12-09 20:22:06 +0900

    translate comments

--- DataStructures/Stacks/StackArray.java
+++ DataStructures/Stacks/StackArray.java
@@ -1,26 +1,26 @@
 /**
- * This class implements a Stack using a regular array.
+ * 이 클래스는 일반 배열을 사용하여 스택을 구현한다.
  * <p>
- * A stack is exactly what it sounds like. An element gets added to the top of
- * the stack and only the element on the top may be removed. This is an example
- * of an array implementation of a Stack. So an element can only be added/removed
- * from the end of the array. In theory stack have no fixed size, but with an
- * array implementation it does.
+ * 스택이 의미하는 것은 무더기이다. 
+ * 스택 맨 위에 요소가 추가되고 맨 위에있는 요소 만 제거 될 수 있습니다.
+ * 이것은 스택의 배열 구현의 예입니다. 
+ * 따라서 배열 끝에서만 요소를 추가/제거 할 수 있습니다. 
+ * 이론적으로 스택의 크기는 고정되어 있지 않지만 배열 구현에서는 크기가 다릅니다.
  *
  * @author Unknown
  */
 public class StackArray {
 
     /**
-     * Main method
+     * 메인 메소드
      *
      * @param args Command line arguments
      */
     public static void main(String[] args) {
-        // Declare a stack of maximum size 4
+        // 스택의 최대 크기 4로 선언한다.
         StackArray myStackArray = new StackArray(4);
 
-        // Populate the stack
+        // 스택을 채우다.
         myStackArray.push(5);
         myStackArray.push(8);
         myStackArray.push(2);
@@ -40,17 +40,17 @@ public class StackArray {
     private static final int DEFAULT_CAPACITY = 10;
 
     /**
-     * The max size of the Stack
+     * 스택의 최대 크기
      */
     private int maxSize;
 
     /**
-     * The array representation of the Stack
+     * 스택의 배열 표현
      */
     private int[] stackArray;
 
     /**
-     * The top of the stack
+     * 스택의 맨 위
      */
     private int top;
 
@@ -62,9 +62,9 @@ public class StackArray {
     }
     
     /**
-     * Constructor
+     * 생성자
      *
-     * @param size Size of the Stack
+     * @param size 스택의 크기
      */
     public StackArray(int size) {
         maxSize = size;
@@ -73,33 +73,33 @@ public class StackArray {
     }
 
     /**
-     * Adds an element to the top of the stack
+     * 스택의 맨위로 요소 추가
      *
-     * @param value The element added
+     * @param value 추가된 요소
      */
     public void push(int value) {
-        if (!isFull()) { // Checks for a full stack
+        if (!isFull()) { // 스택이 가득 차있는지 검사한다.
             top++;
             stackArray[top] = value;
         } else {
             resize(maxSize * 2);
-            push(value); // don't forget push after resizing
+            push(value); // 크기를 바꾼다음에 push할 것
         }
     }
 
     /**
-     * Removes the top element of the stack and returns the value you've removed
+     * 스택에서 맨 위 요소 삭제한 뒤 그 값 반환
      *
-     * @return value popped off the Stack
+     * @return 스택에서 pop된 값
      */
     public int pop() {
-        if (!isEmpty()) { // Checks for an empty stack
+        if (!isEmpty()) { // 빈 스택인지 검사
             return stackArray[top--];
         }
 
         if (top < maxSize / 4) {
             resize(maxSize / 2);
-            return pop();// don't forget pop after resizing
+            return pop();// 크기 바꾼 뒤 pop한다.
         } else {
             System.out.println("The stack is already empty");
             return -1;
@@ -107,12 +107,12 @@ public class StackArray {
     }
 
     /**
-     * Returns the element at the top of the stack
+     * 스택의 맨위 요소를 반환한다.
      *
-     * @return element at the top of the stack
+     * @return 스택의 맨위 요소
      */
     public int peek() {
-        if (!isEmpty()) { // Checks for an empty stack
+        if (!isEmpty()) { // 빈 스택인지 검사
             return stackArray[top];
         } else {
             System.out.println("The stack is empty, cant peek");
@@ -126,38 +126,37 @@ public class StackArray {
         for (int i = 0; i < stackArray.length; i++) {
             transferArray[i] = stackArray[i];
         }
-        // This reference change might be nice in here
+        // 이곳에서 참조변경을 하는 것을 추천한다.
         stackArray = transferArray;
         maxSize = newSize;
     }
 
     /**
-     * Returns true if the stack is empty
+     * 스택이 비어있을 경우 참 반환한다.
      *
-     * @return true if the stack is empty
+     * @return true 빈 스택일 경우
      */
     public boolean isEmpty() {
         return (top == -1);
     }
 
     /**
-     * Returns true if the stack is full
+     * 스택이 가득 차있을 경우 참을 반환
      *
-     * @return true if the stack is full
+     * @return true 스택이 가득 찬 경우
      */
     public boolean isFull() {
         return (top + 1 == maxSize);
     }
 
     /**
-     * Deletes everything in the Stack
+     * 스택에 있는 모든 것을 삭제한다.
      * <p>
-     * Doesn't delete elements in the array
-     * but if you call push method after calling
-     * makeEmpty it will overwrite previous
-     * values
+     * 배열에 있는 요소들 삭제하지 않는다.
+     * makeEmpty 호출 후에 push 메소드를 호출 한다면 덮어쓰기 될 것이다.
+     * 
      */
-    public void makeEmpty() { // Doesn't delete elements in the array but if you call
-        top = -1;             // push method after calling makeEmpty it will overwrite previous values
+    public void makeEmpty() { // 배열에 있는 요소들을 삭제하지 말 것
+        top = -1;             // makeEmpty를 호출한 후 push 메소드를 호출 했다면 전의 값을 덮어쓰기 할 것이다.
     }
 }
