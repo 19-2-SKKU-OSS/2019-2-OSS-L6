8306a08b30fc4be223d8ad7a2749b93e429e3288
bidulki <55395688+bidulki@users.noreply.github.com>
2019-12-10 00:01:30 +0900

    translate comments

--- DataStructures/Trees/LevelOrderTraversalQueue.java
+++ DataStructures/Trees/LevelOrderTraversalQueue.java
@@ -4,10 +4,10 @@ import java.util.Queue;
 import java.util.LinkedList;
 
 
-/* Class to print Level Order Traversal */
+/* 레벨순으로 출력하기 위한 클래스(level order traversal) */
 public class LevelOrderTraversalQueue {
 
-    /* Class to represent Tree node */
+    /* 트리의 노드를 나타내는 클래스*/
     class Node {
         int data;
         Node left, right;
@@ -21,25 +21,26 @@ public class LevelOrderTraversalQueue {
 
     Node root;
 
-    /* Given a binary tree. Print its nodes in level order
-     using array for implementing queue  */
+    /* 주어진 이진 트리에서
+       큐로 구현된 배열을 사용해서 
+       레벨 순으로 노드들을 출력한다  */
     void printLevelOrder() {
         Queue<Node> queue = new LinkedList<Node>();
         queue.add(root);
         while (!queue.isEmpty()) {
  
-            /* poll() removes the present head.
-            For more information on poll() visit 
+            /* poll()는 현제의 head를 제거하는 함수이다.
+            poll()에 대한 더 많은 정보는 아래의 링크를 참고해라
             http://www.tutorialspoint.com/java/util/linkedlist_poll.htm */
             Node tempNode = queue.poll();
             System.out.print(tempNode.data + " ");
 
-            /*Enqueue left child */
+            /*왼쪽 자식을 큐에 넣는다 */
             if (tempNode.left != null) {
                 queue.add(tempNode.left);
             }
 
-            /*Enqueue right child */
+            /*오른쪽 자식을 큐에 넣는다 */
             if (tempNode.right != null) {
                 queue.add(tempNode.right);
             }
